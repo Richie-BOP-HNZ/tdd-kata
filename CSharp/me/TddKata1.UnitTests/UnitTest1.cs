701a4ccc927b3bb61312661b563907ea5a4b4632
Richie Danks <Richie.Danks@TeWhatuOra.govt.nz>
2026-02-17 11:26:01 +1300

    added C# unit tests

---
+++ CSharp/me/TddKata1.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TddKata1.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ CSharp/me/UnitTests.cs
@@ -0,0 +1,13 @@
+#:package xunit.v3@*
+#:project TddKata1.cs
+
+using Xunit;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
