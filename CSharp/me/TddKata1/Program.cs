18a4460e0ecbf8fdd25432d03a7d6617677a85f1
Richie Danks <Richie.Danks@TeWhatuOra.govt.nz>
2026-02-17 13:44:09 +1300

    1st test

--- CSharp/me/TddKata1/Program.cs
+++ CSharp/me/TddKata1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, TDD Kata World!");
+
+int result = Add("0");
+
+Console.WriteLine($"Add: {result}");
+
+static int Add(string numbers)
+{
+    if (string.IsNullOrEmpty(numbers))
+    {
+        return 0;
+    }
+
+    return -1;
+}
