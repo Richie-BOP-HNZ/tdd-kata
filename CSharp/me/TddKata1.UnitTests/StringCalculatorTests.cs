17680851f4727e0648ddb710c2b880273f7a344d
Richie <86538670+Richie-BOP@users.noreply.github.com>
2026-02-17 14:38:18 +1300

    C# - Step 1 & 2 done

---
+++ CSharp/me/TddKata1.UnitTests/StringCalculatorTests.cs
@@ -0,0 +1,89 @@
+namespace TddKata1.UnitTests;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void StringCalculatorEmptyStringReturnsZero()
+    {
+        // Arrange
+        
+        // Act
+        var result = StringCalculator.Add("");
+
+        // Assert
+        Assert.True(result.Equals(0));
+    }
+
+    [Fact]
+    public void StringCalculatorAlphaStringReturnsZero()
+    {
+        // Arrange
+
+        // Act
+        var result = StringCalculator.Add("ABC");
+
+        // Assert
+        Assert.True(result.Equals(0));
+    }
+
+    [Fact]
+    public void StringCalculatorTwoAlphaStringsReturnsZero()
+    {
+        // Arrange
+
+        // Act
+        var result = StringCalculator.Add("ABC,XYZ");
+
+        // Assert
+        Assert.True(result.Equals(0));
+    }
+
+    [Fact]
+    public void StringCalculatorOneReturnsOne()
+    {
+        // Arrange
+
+        // Act
+        var result = StringCalculator.Add("1");
+
+        // Assert
+        Assert.True(result.Equals(1));
+    }
+
+    [Fact]
+    public void StringCalculatorOnePlusTwoReturnsThree()
+    {
+        // Arrange
+
+        // Act
+        var result = StringCalculator.Add("1,2");
+
+        // Assert
+        Assert.True(result.Equals(3));
+    }
+
+    [Fact]
+    public void StringCalculatorOnePlusTwoPlusThreePlusFourReturnsTen()
+    {
+        // Arrange
+
+        // Act
+        var result = StringCalculator.Add("1,2,3,4");
+
+        // Assert
+        Assert.True(result.Equals(10));
+    }
+
+
+    [Fact]
+    public void StringCalculatorOnePlusTwoPlusBlankPlusFourReturnsSeven()
+    {
+        // Arrange
+
+        // Act
+        var result = StringCalculator.Add("1,2,,4");
+
+        // Assert
+        Assert.True(result.Equals(7));
+    }
+}
--- CSharp/me/TddKata1/Program.cs
+++ CSharp/me/TddKata1/Program.cs
@@ -1,16 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, TDD Kata World!");
+﻿namespace TddKata1;
 
-int result = Add("0");
-
-Console.WriteLine($"Add: {result}");
-
-static int Add(string numbers)
+// See https://aka.ms/new-console-template for more information
+internal static class Program
 {
-    if (string.IsNullOrEmpty(numbers))
+    private static void Main(string[] args)
     {
-        return 0;
-    }
+        Console.WriteLine("Hello, TDD Kata 1 in C#!!");
 
-    return -1;
+        int result = StringCalculator.Add("0");
+
+        Console.WriteLine($"Add: {result}");
+        Console.ReadKey();
+    }
+}
+
+public static class StringCalculator
+{
+    public static int Add(string numbers)
+    {
+        // test if numbers is empty, return 0
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
+
+        // test if numbers contains a single number (no comma), return that number
+        if (!numbers.Contains(','))
+        {
+            if(int.TryParse(numbers, out int singleNumber))
+            {
+                return singleNumber;
+            }
+
+            return 0;
+        }
+
+        var splitNumbers = numbers.Split(',');
+
+        // error check: split has no numbers, return 0 - no point looping through an empty array
+        if (splitNumbers.Length == 0)
+        {
+            return 0;
+        }
+
+        int result = 0;
+
+        // loop thru the array and if its a number add to the result
+        foreach (var number in splitNumbers)
+        {
+            if (int.TryParse(number, out int singleNumber))
+            {
+                result += singleNumber;
+            }
+        }
+
+        // return result
+        return result;
+    }
 }
